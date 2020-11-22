6a1a9ec54228c9f4a6a9ed0ddfbd9baa44d62d53
yusufdogan335 <yusufdogann@hotmail.com>
2020-11-23 02:23:39 +0300

    uygulama yapılıyor

---
+++ Loops/script.js
@@ -0,0 +1,7 @@
+/* Uygulama;
+
+1-10 arası rastgele sayı üreten bir sayıyı aşağı yukarı ifadeleri ile buldurmaya çalışın
+
+** puanlama yapın.
+** kullanıcı kaç kerede bileceğini belirtebilirsin. */
+
