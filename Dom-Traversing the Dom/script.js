d28096e7100a7dc69bfa8935fe03bf80140a2d40
yusufdogan335 <yusufdogann@hotmail.com>
2020-11-27 23:31:23 +0300

    exercises done

--- Dom-Traversing the Dom/script.js
+++ Dom-Traversing the Dom/script.js
@@ -32,7 +32,43 @@ let list = document.querySelector('.list-group');
 val = list;
 
 val = list.childNodes;
+val = list.childNodes[0];
+val = list.childNodes[0].nodeName;
+val = list.childNodes[0].nodeType;  //text 
+val = list.childNodes[1].nodeType;  //element
+
+val = list.children;
+val = list.children[0];
+val = list.children[2];
+
+val = list.children[0].textContent = "new item";
+val = list.children[3].children;
+
+val = list.firstChild;
+val = list.firstElementChild;
+val = list.lastChild;
+val = list.lastElementChild;
+
+//eleman sayıları
+
+val = list.childElementCount;
+
+val = list.parentNode;
+val = list.parentElement;
+val = list.parentElement.parentElement;
+
+val = list.children[0].nextSibling;
+val = list.children[0].nextElementSibling;
+
+val = list.children[1].previousSibling;
+val = list.children[1].previousElementSibling;
+
+//console.log(val);
 
 
+for (let i = 0; i < list.children.length; i++){
+    if (list.childNodes[i].nodeType === 3) {
+        console.log(list.childNodes[i]);
+    }  
+}
 
-console.log(val);
