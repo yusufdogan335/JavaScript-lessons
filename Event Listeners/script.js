94529329d48e81f4c2235f4d7e3bc8b4e0a5b212
yusufdogan335 <yusufdogann@hotmail.com>
2020-11-29 21:26:01 +0300

    the subject is understood

--- Event Listeners/script.js
+++ Event Listeners/script.js
@@ -4,13 +4,29 @@ element.addEventListener(olay,fonksiyon,kabarcıklanma); */
 
 
 const btn = document.querySelector('#btnDeleteAll');
+const btn2 = document.querySelector('#btnAddNewTask')
 
-/* btn.addEventListener('click', function () {
+btn.addEventListener('click', function (e) {
     console.log('btn clicked');
-}) */
 
-btn.addEventListener('click', btnClick);
+    let val;
 
-function btnClick() {
+    val = e;
+    val = e.target;
+
+    console.log(val);
+
+    e.preventDefault();
+});
+
+/* btn.addEventListener('click', btnClick2); //tek fonksiyonu 2 butona atayabiliriz
+btn2.addEventListener('click', btnClick);  // yada tek buton için iki tane click olayı atanabilir.
+btn2.addEventListener('click', btnClick); */
+
+
+/* function btnClick() {
     console.log('btn clicked');
 }
+function btnClick2() {
+    console.log('btn clicked');
+} */
